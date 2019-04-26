a7a27e7a57458c2aa5d2c9973dbe63b0b69dfc3e
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-04-25 18:23:48 -0600

    we did the things

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 // IMPORTS
 require('dotenv').config()
 const express = require('express')
+const massive = require('massive')
 const app = express()
-const { SERVER_PORT } = process.env
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 // TOP LEVEL MIDDLEWARE
 app.use(express.json())
 
 // ENDPOINTS
+app.get('/api/house/:houseName', (req, res) => {
+  // const {houseName} = req.params
+  const db = req.app.get('db')
+  db.getHouse({house_name: req.params.houseName}).then(result => {
+    console.log(result)
+    res.status(200).send(result[0])
+  })
+})
 
 // CONNECT TO DATABASE
-
-// START THE SERVER
-app.listen(SERVER_PORT, () => {
-  console.log(`${SERVER_PORT} ducks marching on Rome`)
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set('db', dbInstance)
+  // START THE SERVER
+  app.listen(SERVER_PORT, () => {
+    console.log(`${SERVER_PORT} ducks marching on Rome`)
+  })
 })
--- src/components/House.js
+++ src/components/House.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class House extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      id: 0,
+      house_name: '',
+      description: '',
+      patron_animal: '',
+      img: '',
+      color_1: '',
+      color_2: '',
+      color_3: '',
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    this.getHouseInfo()
+  }
+
+  getHouseInfo() {
+    const {name} = this.props.match.params
+    axios.get(`/api/house/${name}`).then(res => {
+      this.setState(res.data)
+    })
   }
 
   render() {
     return (
       <div>
-        <h1>House</h1>
+        <h1>{this.state.house_name}</h1>
+        <h3>{this.state.description}</h3>
+        <h2>{this.state.patron_animal}</h2>
+        <figure>
+          <img src={this.state.img} alt=""/>
+        </figure>
+        <hr/>
+        <h3>Students</h3>
+        {this.state.students.map((student) => (
+          <h4>{student.name}</h4>
+        ))}
       </div>
     )
   }
